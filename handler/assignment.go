a020c625b9ce4c758e25e3387e17407933a7fddc
ParnThanatibordee <parn8946@gmail.com>
2023-04-10 20:47:19 +0700

    Implement CreateAssignment handler

--- handler/assignment.go
+++ handler/assignment.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/GarnBarn/garnbarn-backend-go/model"
 	"github.com/GarnBarn/garnbarn-backend-go/service"
 	"github.com/gin-gonic/gin"
 )
@@ -14,9 +17,32 @@ func NewAssignmentHandler(rg *gin.RouterGroup, assignmentService service.Assignm
 		assignmentService: assignmentService,
 	}
 
-	// router := rg.Group("/assignment")
+	router := rg.Group("/assignment")
 
-	// router.POST("/", assignmentService.CreateAssignment)
+	router.POST("/", result.CreateAssignment)
 
 	return result
+}
+
+func (a *AssignmentHandler) CreateAssignment(c *gin.Context) {
+	var assignment model.Assignment
+
+	// Check the conditional operator (?:) later.
+	// add validate
+	if err := c.ShouldBindJSON(&assignment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if result := a.assignmentService.CreateAssignment(&assignment); result != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result,
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, &assignment)
+
 }
