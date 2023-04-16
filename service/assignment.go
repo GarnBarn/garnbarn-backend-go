61041a308bb39bd4da228088ff1f043c45095a44
nabhansuwanachote <nabhan.au@gmail.com>
2023-04-16 18:06:35 +0700

    feat: add service for update assignment

--- service/assignment.go
+++ service/assignment.go
@@ -3,12 +3,14 @@ package service
 import (
 	"github.com/GarnBarn/garnbarn-backend-go/model"
 	"github.com/GarnBarn/garnbarn-backend-go/repository"
+	"github.com/sirupsen/logrus"
 )
 
 type AssignmentService interface {
 	CreateAssignment(assignment *model.Assignment) error
 	DeleteAssignment(assignmentId int) error
 	GetAllAssignment(fromPresent bool) ([]model.Assignment, error)
+	UpdateAssignment(updateAssignmentRequest *model.UpdateAssignmentRequest, id int) (*model.Assignment, error)
 }
 
 type assignmentService struct {
@@ -31,4 +33,15 @@ func (a *assignmentService) DeleteAssignment(assignmentId int) error {
 
 func (a *assignmentService) GetAllAssignment(fromPresent bool) ([]model.Assignment, error) {
 	return a.assignmentRepository.GetAllAssignment(fromPresent)
+}
+
+func (a *assignmentService) UpdateAssignment(updateAssignmentRequest *model.UpdateAssignmentRequest, id int) (*model.Assignment, error) {
+	assignment, err := a.assignmentRepository.GetByID(id)
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+	updateAssignmentRequest.UpdateAssignment(assignment)
+	err = a.assignmentRepository.Update(assignment)
+	return assignment, err
 }
